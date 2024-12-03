8a86404acd9f20beb2c5eecdcff5f54a631c5eb3
20k <icedshot@gmail.com>
2024-12-03 17:53:00 +0000

    this is a bit nuts, why can't xmake be normal

--- xmake.lua
+++ xmake.lua
@@ -2,10 +2,48 @@ add_rules("mode.debug", "mode.release")
 add_requireconfs("*", {configs = {shared = false}, system=false})
 
 add_requires("libusb")
+add_packages("libusb")
+
+
+package("rtlsdr_real")
+    add_deps("cmake")
+    set_sourcedir(path.join(os.scriptdir(), "deps/rtl-sdr"))
+    
+    on_install(function (package)
+        local configs = {}
+        table.insert(configs, "-DCMAKE_BUILD_TYPE=" .. (package:debug() and "Debug" or "Release"))
+        table.insert(configs, "-DBUILD_SHARED_LIBS=" .. (package:config("shared") and "ON" or "OFF"))
+        import("package.tools.cmake").install(package, configs)
+    end)
+    
+    --[[
+        on_install(function (package)
+        local configs = {}
+        table.insert(configs, "-DCMAKE_BUILD_TYPE=" .. (package:debug() and "Debug" or "Release"))
+        table.insert(configs, "-DBUILD_SHARED_LIBS=" .. (package:config("shared") and "ON" or "OFF"))
+        import("package.tools.cmake").install(package, configs)
+    end)
+    
+    on_load(function (package)
+        package:set("installdir", path.join(os.scriptdir(), package:plat(), package:arch(), package:mode()))
+    end)
+    
+    on_fetch(function (package)
+        local result = {}
+        result.links = "rtlsdr.dll"
+        result.linkdirs = package:installdir("lib")
+        result.includedirs = package:installdir("include")
+        return result
+    end)
+    --]]
+    
+package_end()
+
+add_requires("rtlsdr_real", {configs={shared=true}})
 
 add_includedirs("./deps")
-add_includedirs("./deps/rtl-sdr/include")
-add_linkdirs("deps/rtl-sdr/build/src")
+--add_includedirs("./deps/rtl-sdr/include")
+--add_linkdirs("deps/rtl-sdr/build/src")
 
 set_languages("c99", "cxx23")
 
@@ -15,7 +53,7 @@ add_cxxflags("-fexperimental-library")
 set_optimize("fastest")
 
 if is_plat("windows", "mingw", "msys") then
-    add_syslinks("ws2_32", "winmm", "rtlsdr.dll")
+    add_syslinks("ws2_32", "winmm")
 else
     add_syslinks("rtlsdr")
 end
@@ -25,6 +63,7 @@ if is_plat("msys", "mingw") then
 end
 
 target("rtl_daemon")
+    add_packages("rtlsdr_real")
     set_kind("binary")
     add_files("main.cpp")
     
